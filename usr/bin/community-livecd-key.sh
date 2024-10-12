4cc90983f039316e26b69a5d658ae01ef5d35eaf
talesam <talesam@gmail.com>
2024-10-11 22:26:51 -0300

    Remoção da Gambiarra

--- usr/bin/community-livecd-key.sh
+++ usr/bin/community-livecd-key.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Gambiarra
-if [[ -e /run/miso/sfs/livefs && -e /usr/bin/gdm ]]; then
-    echo 'default_desktop_file=livecdWayland' >> /etc/manjaro-tools/live.conf
-fi
-
 test ! -d /etc/pacman.d/gnupg && mkdir -p /etc/pacman.d/gnupg
 test ! -e /etc/pacman.d/gnupg/pubring.gpg && cp -a /usr/share/pacman/keyrings/pubring.gpg /etc/pacman.d/gnupg/pubring.gpg
 test ! -e /etc/pacman.d/gnupg/trustdb.gpg && cp -a /usr/share/pacman/keyrings/trustdb.gpg /etc/pacman.d/gnupg/trustdb.gpg
