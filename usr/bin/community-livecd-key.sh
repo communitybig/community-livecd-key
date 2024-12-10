e2323408ad79f1ea1a91316edc204358659b2377
talesam <talesam@gmail.com>
2024-12-10 14:45:34 -0300

    Update systemd

--- usr/bin/community-livecd-key.sh
+++ usr/bin/community-livecd-key.sh
@@ -3,5 +3,3 @@
 test ! -d /etc/pacman.d/gnupg && mkdir -p /etc/pacman.d/gnupg
 test ! -e /etc/pacman.d/gnupg/pubring.gpg && cp -a /usr/share/pacman/keyrings/pubring.gpg /etc/pacman.d/gnupg/pubring.gpg
 test ! -e /etc/pacman.d/gnupg/trustdb.gpg && cp -a /usr/share/pacman/keyrings/trustdb.gpg /etc/pacman.d/gnupg/trustdb.gpg
-
-
