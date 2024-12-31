c911ef30e6157e544091d27d1caaf654ad42876e
vcatafesta <vcatafesta@gmail.com>
2024-12-31 16:30:46 -0400

    ter 31 dez 2024 16:30:46 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- usr/bin/community-livecd-key.sh
+++ usr/bin/community-livecd-key.sh
@@ -1,5 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-test ! -d /etc/pacman.d/gnupg && mkdir -p /etc/pacman.d/gnupg
-test ! -e /etc/pacman.d/gnupg/pubring.gpg && cp -a /usr/share/pacman/keyrings/pubring.gpg /etc/pacman.d/gnupg/pubring.gpg
-test ! -e /etc/pacman.d/gnupg/trustdb.gpg && cp -a /usr/share/pacman/keyrings/trustdb.gpg /etc/pacman.d/gnupg/trustdb.gpg
+# Lista dos arquivos a serem copiados
+files=('pubring.gpg' 'trustdb.gpg')
+
+# Caminhos de origem e destino
+srcpath='/usr/share/pacman/keyrings'
+destpath='/etc/pacman.d/gnupg'
+
+# Verifica se o diretório de destino existe, caso contrário, cria-o
+[[ ! -d "$destpath" ]] && mkdir -p "$destpath"
+
+# Itera pelos arquivos da lista
+for file in "${files[@]}"; do
+	srcfile="$srcpath/$file"
+	destfile="$destpath/$file"
+
+	# Verifica se o arquivo existe na origem
+	if [[ -e "$srcfile" ]]; then
+    # Copia o arquivo apenas para o destino
+    cp -af "$srcfile" "$destfile" # Copia o arquivo
+  fi
+done
