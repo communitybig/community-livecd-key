12246b6677bee473bd96f8093144468002e8aa2f
vcatafesta <vcatafesta@gmail.com>
2025-01-01 13:04:26 -0400

    qua 01 jan 2025 13:04:26 -04 Vilmar Catafesta (vcatafesta@gmail.com)

--- usr/bin/community-livecd-key.sh
+++ usr/bin/community-livecd-key.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+msg() {
+  local ALL_OFF="\e[1;0m"
+  local BOLD="\e[1;1m"
+  local GREEN="${BOLD}\e[1;32m"
+
+  local MESSAGE=$1
+  shift 1
+  printf "${GREEN}==>[community-livecd-key]${ALL_OFF}${BOLD} ${MESSAGE}${ALL_OFF}\n" "$@" >&2
+}
+
 # Lista dos arquivos a serem copiados
 files=('pubring.gpg' 'trustdb.gpg')
 
@@ -17,10 +27,12 @@ for file in "${files[@]}"; do
 
 	# Verifica se o arquivo existe na origem
 	if [[ -e "$srcfile" ]]; then
-    # Copia o arquivo apenas para o destino
-    cp -af "$srcfile" "$destfile" # Copia o arquivo
+    msg "Copying 'Public Keyring' and 'Trust Database' ${srcfile} into ${destfile}..."
+    cp -af "$srcfile" "$destfile" >/dev/null 2>&1
   fi
 done
 
 # Garante que o chaveiro está inicializado corretamente
-pacman-key --init
+msg "Initializing pacman keyring..."
+pacman-key --init >/dev/null 2>&1
+msg "Done!"
